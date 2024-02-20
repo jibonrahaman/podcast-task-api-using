624dd3d7056dbf4659cb6a093de1e5dff158ead6
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-20 12:02:58 +0600

    withoutLogged page

---
+++ src/Layout/WithOutLogged/WithLogged.jsx
@@ -0,0 +1,13 @@
+
+//    with out login page ------------
+function WithLogged() {
+  return (
+   <section className="withoutLogged h-screen bg-[#3B3B3B] w-full">
+      <div className="">
+
+      </div>
+   </section>
+  )
+}
+
+export default WithLogged
