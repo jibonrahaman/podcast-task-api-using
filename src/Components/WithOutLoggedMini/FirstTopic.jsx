baccaa41fe57d3c2cb81d9e4bc1c6e37c99ab403
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-20 22:55:21 +0600

    sign up  popup design

--- src/Components/WithOutLoggedMini/FirstTopic.jsx
+++ src/Components/WithOutLoggedMini/FirstTopic.jsx
@@ -10,7 +10,7 @@ function FirstTopic({className}) {
        <h2 className=" text-3xl font-semibold"> Welcome to fauget music services</h2>
       
        <div className=" flex  gap-x-20 ">       
-       {/* popup ----------------------- */}
+       {/* popup sign in ----------------------- */}
        <Popup 
     trigger={<button className="px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-black hover:bg-[#434040]">Sign in </button>}
     modal
@@ -42,8 +42,44 @@ function FirstTopic({className}) {
         </div>
     )}
   </Popup>
-      {/* popup ----------------------- */}
-       <button className=" px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-[#434040] hover:bg-black">Sign up</button>
+      {/* popup sign in----------------------- */}
+
+      {/* popup sign up ----------------------- */}
+      <Popup 
+    trigger={ <button className=" px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-[#434040] hover:bg-black">Sign up</button>}
+    modal
+    nested
+  >
+    {close => (
+      <div className="modal ">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header flex flex-col items-center text-center">
+          <Images src={logo} />
+          <h2 className=' text-white text-2xl '>Sign </h2>
+          </div> 
+
+          <div  className='text-white'>
+          <p className='  text-xl text-left'>Email:</p>
+          <input type="text" placeholder='enter your email' className=' px-16 py-1 rounded-md bg-black outline-none mt-3 mb-5' />
+
+          <p className='  text-xl text-left'>Password:</p>
+          <input type="text" placeholder='enter your password' className=' px-16 py-1 rounded-md bg-black outline-none my-3' />
+
+          <button className='my-3 px-10 py-2 bg-black  rounded-xl duration-300 hover:bg-[#262525]'>Create an account</button>
+          <p className=' text-lg mt-3'> Don’t have account? Create an account </p>
+          <button className=' text-red-700 text-center'>sign in</button>
+          </div> 
+          
+                
+        </div>
+    )}
+  </Popup>
+      {/* popup sign up ----------------------- */}
+
+
+      
        
        </div>
     </div>
