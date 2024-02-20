011a9e535c3f7a4c358d1bf4a29ede34e2071bf4
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-20 22:52:16 +0600

    sign up popup design

--- src/Components/WithOutLoggedMini/FirstTopic.jsx
+++ src/Components/WithOutLoggedMini/FirstTopic.jsx
@@ -9,35 +9,37 @@ function FirstTopic({className}) {
      <div className={`text-white flex justify-between  pt-6 ${className}`}>
        <h2 className=" text-3xl font-semibold"> Welcome to fauget music services</h2>
       
-       <div className=" flex  gap-x-20">       
+       <div className=" flex  gap-x-20 ">       
        {/* popup ----------------------- */}
-       <Popup
+       <Popup 
     trigger={<button className="px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-black hover:bg-[#434040]">Sign in </button>}
     modal
     nested
   >
     {close => (
-      <div className="modal">
+      <div className="modal ">
         <button className="close" onClick={close}>
           &times;
         </button>
         <div className="header flex flex-col items-center text-center">
           <Images src={logo} />
           <h2 className=' text-white text-2xl '>Sign </h2>
-          <p>Email:</p>
-          <input type="text" placeholder='enter your email' className=' px-6 py-1 rounded-md bg-black outline-none' />
-           </div>
-        
-        {/* <div className="actions">
-          <Popup
-           
+          </div> 
+
+          <div  className='text-white'>
+          <p className='  text-xl text-left'>Email:</p>
+          <input type="text" placeholder='enter your email' className=' px-16 py-1 rounded-md bg-black outline-none mt-3 mb-5' />
+
+          <p className='  text-xl text-left'>Password:</p>
+          <input type="text" placeholder='enter your password' className=' px-16 py-1 rounded-md bg-black outline-none my-3' />
+
+          <button className='my-3 px-10 py-1 bg-black  rounded-xl duration-300 hover:bg-[#262525]'>Sign in</button>
+          <p className=' text-lg mt-3'> Don’t have account? Create an account </p>
+          <button className=' text-red-700 text-center'>signup</button>
+          </div> 
           
-          >
-            
-          </Popup>
-         
-        </div> */}
-      </div>
+                
+        </div>
     )}
   </Popup>
       {/* popup ----------------------- */}
