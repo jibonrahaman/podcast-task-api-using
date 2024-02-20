74f383b62ba1671d77174c6dc73d28a401881658
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-20 16:55:19 +0600

    ThirdButton show

--- src/Components/ThirdButton.jsx
+++ src/Components/ThirdButton.jsx
@@ -1,6 +1,6 @@
 
 export default function ThirdButton({className,text}) {
   return (
-    <button className={`px-4 py-1 rounded-lg ${className}`}>{text}</button>
+    <button className={`px-8 text-center py-1 text-lg font-semibold rounded-lg text-white bg-[#434040] ${className}`}>{text}</button>
   )
 }
--- src/Components/WithOutLoggedMini/ThirdTopic.jsx
+++ src/Components/WithOutLoggedMini/ThirdTopic.jsx
@@ -1,8 +1,18 @@
+import Flex from "../Flex";
+import ThirdButton from "../ThirdButton";
 
 export default function ThirdTopic() {
   return (
-    <div>
-      
-    </div>
+ <div className=" mt-8">
+       <Flex className=" justify-between">
+      <ThirdButton  text="For You"/>
+      <ThirdButton  text="Popular"/>
+      <ThirdButton  text="Trend"/>
+      <ThirdButton  text="Pop"/>
+      <ThirdButton  text="Edm"/>
+      <ThirdButton  text="Rock"/>
+      <ThirdButton  text="More"/>
+    </Flex>
+ </div>
   )
 }
