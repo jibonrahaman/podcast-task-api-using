6a4baedefec80dca9980ac0e729974ea20c516a4
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-20 23:35:28 +0600

    Logged First Topic  input voice & search button position

--- src/Components/Logged/LoggedFirstTopic.jsx
+++ src/Components/Logged/LoggedFirstTopic.jsx
@@ -6,10 +6,11 @@ function LoggedFirstTopic({className}) {
  <section>
      <div className={`text-white flex justify-between  pt-6 ${className}`}>
        <h2 className=" text-3xl font-semibold"> Welcome to fauget music services</h2>      
-       <input type="text" placeholder="Artist, Music, Album, Etc"/>
-       <FaSearch />
-       <MdOutlineKeyboardVoice />
-
+       <div className=" relative">
+       <input type="text" placeholder="Artist, Music, Album, Etc" className="py-2 px-10 bg-black rounded-3xl"/>
+       <FaSearch className=" absolute top-[13px] left-4" />
+       <MdOutlineKeyboardVoice className=" absolute top-3 right-5"/>
+       </div>
     </div>          
  </section>
   )
