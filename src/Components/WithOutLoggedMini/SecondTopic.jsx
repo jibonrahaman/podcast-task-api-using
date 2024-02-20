7b8fad16f0b0767abb72e8c4006482c41885e5ac
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-20 17:14:44 +0600

    withoutLogged design done

--- src/Components/WithOutLoggedMini/SecondTopic.jsx
+++ src/Components/WithOutLoggedMini/SecondTopic.jsx
@@ -4,10 +4,12 @@ export default function SecondTopic() {
     return (
         <div className="text-lg font-semibold mt-5 text-white ">
             <div className=" relative ">
-                <Images src={WithLogged2ndTopic} alt={WithLogged2ndTopic} className=""/>
-                <div className=" absolute top-12 left-14 ">
+               <div className=" h-[280px]">
+               <Images src={WithLogged2ndTopic} alt={WithLogged2ndTopic} className=" w-full h-full"/>
+               </div>
+                <div className=" absolute top-10 left-14 ">
                     <p>Top play music station</p>
-                    <h2 className=" text-3xl my-3">The Dark Side of Music: Unveiling Its Negative Effects</h2>
+                    <h2 className=" text-2xl my-3">The Dark Side of Music: Unveiling Its Negative Effects</h2>
                     <p className=" w-[50%] my-3 text-sm font-medium">Music, often regarded as a universal language, possesses a profound impact on individuals and societies alike. While it has long been celebrated for its ability to evoke emotions, foster connections, and uplift spirits, the flip side of the melody reveals a spectrum of adverse effects that are often overlooked</p>
                     <button className=" px-6 py-1 bg-[#11343c] rounded-xl">For You?</button>
                 </div>
--- src/Components/WithOutLoggedMini/ThirdTopic.jsx
+++ src/Components/WithOutLoggedMini/ThirdTopic.jsx
@@ -6,9 +6,11 @@ import musicTwo from '/public/images/music2.png';
 import musicThree from '/public/images/music3.png';
 import musicFour from '/public/images/music4.png';
 import musicFive from '/public/images/music5.png';
+import { FaRegPlayCircle } from "react-icons/fa";
+
 export default function ThirdTopic() {
   return (
- <div className=" mt-8">
+ <div className=" mt-16">
        <Flex className=" justify-between">
       <ThirdButton  text="For You"/>
       <ThirdButton  text="Popular"/>
@@ -20,11 +22,31 @@ export default function ThirdTopic() {
     </Flex>
      
      <Flex className=" mt-5 justify-between">
-        <Images src={musicOne} alt={musicOne} />
-        <Images src={musicTwo} alt={musicTwo} />
-        <Images src={musicThree} alt={musicThree} />
-        <Images src={musicFour} alt={musicFour} />
-        <Images src={musicFive} alt={musicFive} />
+      <div className=" relative">
+      <Images src={musicOne} alt={musicOne} />
+      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+      </div>
+
+      <div className=" relative">
+      <Images src={musicTwo} alt={musicTwo} />
+      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+      </div>
+
+      <div className=" relative">
+      <Images src={musicThree} alt={musicThree} />
+      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+      </div>
+
+      <div className=" relative">
+      <Images src={musicFour} alt={musicFour} />
+      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+      </div>
+
+      <div className=" relative">
+      <Images src={musicFive} alt={musicFive} />
+      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+      </div>
+        
      </Flex>
 
  </div>
