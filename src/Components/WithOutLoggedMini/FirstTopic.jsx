00759daf906f366e1176a70f320aea752812d291
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-22 12:27:41 +0600

    set  input data

--- src/Components/WithOutLoggedMini/FirstTopic.jsx
+++ src/Components/WithOutLoggedMini/FirstTopic.jsx
@@ -4,79 +4,80 @@ import logo from '/public/images/logo.png'
 import { IoCloseCircleOutline } from "react-icons/io5";
 
 function FirstTopic({ className }) {
-  
-  const [SignInPopup,setSignInPopup] =useState(false)
-  const [SignupPopup,setSignupPopup] =useState(false)
-  
-const handleSignInPopup = () =>{
-  setSignInPopup(true)
-}
-const handleSignUpPopup = () =>{
-  setSignupPopup(true)
-}
-const handleCloseSignIn = () =>{
-  setSignInPopup(false)
-  setSignupPopup(true)
-}
-const handleCloseSignUp = () =>{
-  setSignInPopup(true)
-  setSignupPopup(false)
-}
+  const [SignInPopup, setSignInPopup] = useState(false)
+  const [SignupPopup, setSignupPopup] = useState(false)
+  const [singUpEmail,setsingUpEmail] =useState('')
+  const [singUpPass,setsingUpPass] =useState('')
+
+  const handleSignInPopup = () => {
+    setSignInPopup(true)
+  }
+  const handleSignUpPopup = () => {
+    setSignupPopup(true)
+  }
+  const handleCloseSignIn = () => {
+    setSignInPopup(false)
+    setSignupPopup(true)
+  }
+  const handleCloseSignUp = () => {
+    setSignInPopup(true)
+    setSignupPopup(false)
+  }
 
 
   return (
-    <section  className=' relative'>
+    <section className=' relative'>
       <div className={`text-white flex justify-between pt-6 ${className}`}>
-        
+
         <h2 className=" text-3xl font-semibold"> Welcome to fauget music services</h2>
-        <div className=" flex  gap-x-20 ">   
-         <button  onClick={handleSignInPopup} className="px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-black hover:bg-[#434040]">Sign in </button>
-         <button onClick={handleSignUpPopup} className=" px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-[#434040] hover:bg-black">Sign up</button>     
-       </div>
+        <div className=" flex  gap-x-20 ">
+          <button onClick={handleSignInPopup} className="px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-black hover:bg-[#434040]">Sign in </button>
+          <button onClick={handleSignUpPopup} className=" px-10 py-2 cursor-pointer text-center duration-300 rounded-xl bg-[#434040] hover:bg-black">Sign up</button>
+        </div>
 
         {
-        SignInPopup &&
-        <div className='absolute top-0 left-0 w-full h-screen bg-black/10 z-50 backdrop-blur-sm flex justify-center items-center '>
-          
+          SignInPopup &&
+          <div className='absolute top-0 left-0 w-full h-screen bg-black/10 z-50 backdrop-blur-sm flex justify-center items-center '>
 
-        <div className=" relative w-[400px] flex flex-col items-center text-center bg-[#3b3b3b] rounded-lg p-5">
-        <button onClick={()=>setSignInPopup(false)} className=" cursor-pointer text-4xl absolute top-[-5px] right-[-5px]">
-            <IoCloseCircleOutline  />
-            </button>
-          <Images src={logo} />
-          <h2 className='text-white text-2xl'>Sign</h2>
-          <div className='text-white'>
-            <p className='text-xl mt-3 '>Email:</p>
-            <input type="text" placeholder='enter your email' className='px-16 py-1 rounded-md bg-black outline-none mt-1  mb-5' />
-            <p className='text-xl '>Password:</p>
-            <input type="text" placeholder='enter your password' className='px-16 py-1 rounded-md bg-black outline-none  mt-1' />
-            <button  className='my-5 px-10 py-2 bg-black rounded-xl duration-300 hover:bg-[#262525]'>Sign in</button>
-            <p className='text-lg mt-3'>Don’t have an account? <button onClick={handleCloseSignIn} className='text-red-700 text-center'>Create an account</button></p>
+
+            <div className=" relative w-[400px] flex flex-col items-center text-center bg-[#3b3b3b] rounded-lg p-5">
+              <button onClick={() => setSignInPopup(false)} className=" cursor-pointer text-4xl absolute top-[-5px] right-[-5px]">
+                <IoCloseCircleOutline />
+              </button>
+              <Images src={logo} />
+              <h2 className='text-white text-2xl'>Sign</h2>
+              <div className='text-white'>
+                <p className='text-xl mt-3 '>Email:</p>
+                <input type="text" placeholder='enter your email' className='px-16 py-1 rounded-md bg-black outline-none mt-1  mb-5' />
+                <p className='text-xl '>Password:</p>
+                <input type="text" placeholder='enter your password' className='px-16 py-1 rounded-md bg-black outline-none  mt-1' />
+                <button className='my-5 px-10 py-2 bg-black rounded-xl duration-300 hover:bg-[#262525]'>Sign in</button>
+                <p className='text-lg mt-3'>Don’t have an account? <button onClick={handleCloseSignIn} className='text-red-700 text-center'>Create an account</button></p>
+              </div>
+            </div>
           </div>
-        </div>
-        </div>
-      } 
-      
-      {
-        SignupPopup &&
-        <div className='absolute top-0 left-0 w-full h-screen bg-black/10 z-50 backdrop-blur-sm flex justify-center items-center '>
-        <div className=" relative w-[400px] flex flex-col items-center text-center bg-[#3b3b3b] rounded-lg p-5">
-        <button onClick={()=>setSignupPopup(false)} className=" cursor-pointer text-4xl absolute top-[-5px] right-[-5px]">
-            <IoCloseCircleOutline  />
-            </button>
-          <Images src={logo} />
-          <h2 className='text-white text-2xl'>Sign</h2>
-          <div className='text-white'>
-            <p className='text-xl mt-3 '>Email:</p>
-            <input type="text" placeholder='enter your email' className='px-16 py-1 rounded-md bg-black outline-none mt-1  mb-5' />
-            <p className='text-xl '>Password:</p>
-            <input type="text" placeholder='enter your password' className='px-16 py-1 rounded-md bg-black outline-none  mt-1' />
-            <button  className='my-5 px-10 py-2 bg-black rounded-xl duration-300 hover:bg-[#262525]'>Create an account</button>
-            <p className='text-lg mt-3'> Have account? Sign in  account  <button onClick={handleCloseSignUp} className='text-red-700 text-center'> signin</button></p>
+        }
+
+        {
+          SignupPopup &&
+          <div className='absolute top-0 left-0 w-full h-screen bg-black/10 z-50 backdrop-blur-sm flex justify-center items-center '>
+            <div className=" relative w-[400px] flex flex-col items-center text-center bg-[#3b3b3b] rounded-lg p-5">
+              <button onClick={() => setSignupPopup(false)} className=" cursor-pointer text-4xl absolute top-[-5px] right-[-5px]">
+                <IoCloseCircleOutline />
+              </button>
+              <Images src={logo} />
+              <h2 className='text-white text-2xl'>Sign</h2>
+              <div className='text-white'>
+                <p className='text-xl mt-3 '>Email:</p>
+                <input type="text" onChange={(e)=>setsingUpEmail(e.target.value)} value={singUpEmail} placeholder='enter your email' className='px-16 py-1 rounded-md bg-black outline-none mt-1  mb-5' />
+                <p className='text-xl '>Password:</p>
+                <input type="text" onChange={(e)=>setsingUpPass(e.target.value)} value={singUpPass} placeholder='enter your password' className='px-16 py-1 rounded-md bg-black outline-none  mt-1' />
+                <button className='my-5 px-10 py-2 bg-black rounded-xl duration-300 hover:bg-[#262525]'>Create an account</button>
+                <p className='text-lg mt-3'> Have account? Sign in  account  <button onClick={handleCloseSignUp} className='text-red-700 text-center'> signin</button></p>
+              </div>
+            </div>
           </div>
-        </div>
-        </div>
-      }
+        }
       </div>
     </section>
   )
