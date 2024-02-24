b858e5d868e7fe527d3d67050941ce8894b93167
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-24 21:53:14 +0600

    Check if userData is empty or undefined not navigate logged page

--- src/Pages/Logged.jsx
+++ src/Pages/Logged.jsx
@@ -1,14 +1,19 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import LoggedFirstTopic from "../Components/Logged/LoggedFirstTopic";
 import LoggedSecondTopic from "../Components/Logged/LoggedSecondTopic";
 import LoggedThirdTopic from "../Components/Logged/LoggedThirdTopic";
-// import { useSelector } from "react-redux";
-// import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function Logged() {
-  // const navigate =useNavigate();
-  // const userData =useSelector(state => state.userLoginInfo.userInfo);
- 
+  const navigate =useNavigate();
+  const userData =useSelector(state => state.userLoginInfo.userInfo);
+  useEffect(() => {
+    // Check if userData is empty or undefined
+    if (!userData || userData.length == 0) {
+      navigate('/');
+    }
+  }, [userData,navigate]);
   return (
     <div className=' bg-[#191919] w-full px-16'>
       <LoggedFirstTopic />
