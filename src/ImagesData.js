693bcd6906b96a833107a6fbc81b0f9b81dcea24
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-25 20:55:11 +0600

    ImagesData map and show all images

---
+++ src/ImagesData.js
@@ -0,0 +1,38 @@
+const ImagesData = [
+    {
+      "id": 1,
+      "title": "For You",
+      "imgs": [
+        "/public/images/music1.png",
+        "/public/images/music2.png",
+        "/public/images/music3.png",
+        "/public/images/music4.png",
+        "/public/images/music5.png"
+      ]
+    },
+    {
+        "id": 2,
+        "title": "Popular",
+        "imgs": [
+          "/public/images/music1.png",
+          "/public/images/music2.png",
+          "/public/images/music3.png",
+          "/public/images/music4.png",
+          "/public/images/music5.png"
+        ]
+      },
+      {
+        "id": 3,
+        "title": "Trend",
+        "imgs": [
+          "/public/images/music1.png",
+          "/public/images/music2.png",
+          "/public/images/music3.png",
+          "/public/images/music4.png",
+          "/public/images/music5.png"
+        ]
+      },
+  ];
+  
+  export default ImagesData;
+  
--- src/Pages/PodCast.jsx
+++ src/Pages/PodCast.jsx
@@ -9,20 +9,41 @@ import { FaRegPlayCircle } from "react-icons/fa";
 import Flex from "../Components/Flex";
 import Images from "../Components/Images";
 import ThirdButton from "../Components/ThirdButton";
+import { useState } from "react";
+import ImagesData from "../ImagesData";
 export default function PodCast() {
+  const [data, setData] = useState(ImagesData);
   return (
     <section className=' bg-[#191919] w-full px-16 '>
-         <div className={`text-white flex justify-between  pt-6 `}>
-       <h2 className=" text-3xl font-semibold"> Welcome,claudia Alvies</h2>      
-       <div className=" relative">
-       <input type="text" placeholder="Artist, Music, Album, Etc" className="py-2 px-10 bg-black rounded-3xl"/>
-       <FaSearch className=" absolute top-[13px] left-4" />
-       <MdOutlineKeyboardVoice className=" absolute top-3 right-5"/>
-       </div>
-    </div> 
+      <div className={`text-white flex justify-between  pt-6 `}>
+        <h2 className=" text-3xl font-semibold"> Welcome,claudia Alvies</h2>
+        <div className=" relative">
+          <input type="text" placeholder="Artist, Music, Album, Etc" className="py-2 px-10 bg-black rounded-3xl" />
+          <FaSearch className=" absolute top-[13px] left-4" />
+          <MdOutlineKeyboardVoice className=" absolute top-3 right-5" />
+        </div>
+      </div>
 
-  <div className="overflow-y-scroll w-full h-[600px]">
-  <div className="mt-10 ">
+      <div className="overflow-y-scroll w-full h-[600px]">
+        {
+          data.map((item) => {
+            const { id, title, imgs } = item
+            return <div key={id} className="mt-10 ">
+              <ThirdButton text={title} />
+              <Flex className=" mt-5 justify-between">
+                {
+                  imgs.map((items, index) => (
+                    <div key={index} className=" relative">
+                      <Images src={items} alt={items} />
+                      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+                    </div>
+                  ))
+                }
+              </Flex>
+            </div>
+          })
+        }
+        {/* <div className=" mt-10">
       <ThirdButton  text="For You"/>
       <Flex className=" mt-5 justify-between">
       <div className=" relative">
@@ -83,39 +104,8 @@ export default function PodCast() {
         
      </Flex>
 
- </div>
- <div className=" mt-10">
-      <ThirdButton  text="For You"/>
-      <Flex className=" mt-5 justify-between">
-      <div className=" relative">
-      <Images src={musicOne} alt={musicOne} />
-      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
+ </div> */}
       </div>
-
-      <div className=" relative">
-      <Images src={musicTwo} alt={musicTwo} />
-      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
-      </div>
-
-      <div className=" relative">
-      <Images src={musicThree} alt={musicThree} />
-      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
-      </div>
-
-      <div className=" relative">
-      <Images src={musicFour} alt={musicFour} />
-      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
-      </div>
-
-      <div className=" relative">
-      <Images src={musicFive} alt={musicFive} />
-      <FaRegPlayCircle className=" absolute top-[50%] text-white left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl" />
-      </div>
-        
-     </Flex>
-
- </div>
-  </div>
     </section>
   )
 }
